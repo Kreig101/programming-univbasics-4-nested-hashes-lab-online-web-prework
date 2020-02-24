b6385e3c68758ef909c3086507848d47520e8497
Craig Storer <craigstorer@gmail.com>
2020-02-24 16:50:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,6 +136,6 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].join = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 
 end
